cc3f0feae3c6c8bd05522214af531de2872209f7
Kramar Nadezhda <nadezhda.kramar@telekom.com>
2022-05-25 09:36:33 +0300

    first commit

---
+++ src/test/java/guru/qa/FirstTest.java
@@ -0,0 +1,17 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+  @Test
+  void firstTest() {
+    Assertions.assertTrue(3 >2);
+  }
+
+  @Test
+  void secondTest() {
+    Assertions.assertTrue(3 >1);
+  }
+}
+
